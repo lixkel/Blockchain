40da6539b0565ca5a5605d9cfffa041312f16ac3
M0jo91 <mojo20002303@gmail.com>
2020-08-18 22:05:29 +0200

    sqlite

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,17 @@
-import mongodb
+import sqlite3
 from time import time
 import hashlib
 
 
-class blockchain:
-
+class Blockchain:
     def __init__(self):
-        
+        self.conn = sqlite3.connect("blockchain.db")
+        self.c = self.conn.cursor()
+        self.mempool = []
+        try:
+            self.c.execute("SELECT * FROM blockhain")
+        except:
+            c.execute("""CREATE TABLE test (
+                hash text,
+                block text)
+                """)
