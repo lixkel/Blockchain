babcdcadcadfc7d08773af89a76ba037b5a61324
M0jo91 <mojo20002303@gmail.com>
2020-08-21 00:45:28 +0200

    ecdsa

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import sqlite3
 from time import time
 import hashlib
+from ecdsa import SigningKey, SECP256k1
 
 
 class Blockchain:
@@ -12,6 +13,17 @@ class Blockchain:
             self.c.execute("SELECT * FROM blockhain")
         except:
             c.execute("""CREATE TABLE test (
+                id integer,
                 hash text,
                 block text)
                 """)
+        try:
+            keyFile = open("keyFile", "r")
+            read = bytes.fromhex(keyFile.read())
+            self.key = SigningKey.from_string(read, curve=SECP256k1)
+        except:
+            keyFile = open("keyFile", "w")
+            self.key = SigningKey.generate(curve=SECP256k1)
+            keyFile.write(SigningKey.to_string().hex())
+        keyFile.close()
+        self.ver_key = self.key.verifying_key
